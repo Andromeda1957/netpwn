be9552756b9ffa954813d25c26a45560c2789f93
3XPL017 <eclipse0010101@gmail.com>
2018-05-08 17:29:03 -0500

    Add files via upload

---
+++ resources/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+__path__ = ['resources']
+__all__ = [filename.rsplit('.')[0] for filename in os.listdir(__path__[0])
+           if not filename.startswith('_')
+           and (filename.endswith('.py') or os.path.isdir("{}/{}".format(__path__[0], filename)))]
